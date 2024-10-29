d6538333c68d40a3bda131d50cf13aa827597fb7
Nato <rponcel@unsa.edu.pe>
2024-10-29 16:10:42 -0500

    Añadiendo el cgi para realizar las operaciones

--- cgi-bin/calculadora.pl
+++ cgi-bin/calculadora.pl
@@ -3,3 +3,42 @@
 use strict;
 use warnings;
 use CGI;
+
+my $calcular = CGI -> new;
+my $operacion = $calcular->param('calcular');
+my $resultado;
+
+if ($operacion =~ /^d+\+\-\*\/]\d+*$/){
+    $resultado=calcular($operacion);
+    print "<h3>El resultado $operacion es $resultado<h3>";
+} else {
+    print "<h3>Hubo un error en el resultado, revise su operación.<h3>";
+}
+
+sub calcular{
+    my ($operacion)=@_;
+    my $resultado=0;
+    #Realizamos primero las multiplicaciones y las divisiones
+    while ($operacion =~ /(-?\d+)([\*\/])(\d+)/){
+        my ($primero, $operador, $segundo) = ($1, $2, $3);
+        if ($temporal eq (*)){
+	    $temporal = $primero * $segundo;
+	} else {
+	    $temporal = $primero / $segundo;
+	}
+        $resultado+=$operacion;
+    }
+
+    #Realizamos después las sumas y las restas
+    while ($operacion =~ /(-?\d+)([\+\-])(\d+)/){
+        my ($primero, $operador, $segundo) = ($1, $2, $3);
+	if ($temporal eq (+)){
+	    $temporal = $primero + $segundo;
+	} else {
+	    $temporal = $primero - $segundo;
+	}
+        $resultado+=$operacion;
+    }
+
+    return $operacion;
+}
