edf659552c2ab772ac0109fb52607fc35597ddf9
Nato <rponcel@unsa.edu.pe>
2024-10-30 18:18:59 -0500

    Se mejora el manejo de decimales para el resultado

--- cgi-bin/calculadora.pl
+++ cgi-bin/calculadora.pl
@@ -22,17 +22,29 @@ print $calcular -> end_html;
 sub calcular{
     my ($operacion)=@_;
 
-    #Realizamos primero las multiplicaciones y las divisiones
-    while ($operacion =~ /(-?\d+)([\*\/])(\d+)/){
-        my ($primero, $operador, $segundo) = ($1, $2, $3);
+    # Se realizan las multiplicaciones y las divisiones primero
+    while ($operacion =~ /(-?\d+)(\.\d+)?([\*\/])(-?\d+)(\.\d+)?/) {
+        my ($primero, $operador, $segundo) = ($1, $3, $4);
+        if (defined $2){
+            $primero.=$2;
+        }
+        if (defined $5){
+            $segundo.=$5;
+        }
         my $temporal = ($operador eq '*') ? $primero * $segundo : $primero / $segundo;
         $operacion =~ s/\Q$primero$operador$segundo\E/$temporal/;
     }
 
-    #Realizamos después las sumas y las restas
-    while ($operacion =~ /(-?\d+)([\+\-])(\d+)/){
-        my ($primero, $operador, $segundo) = ($1, $2, $3);
-	my $temporal = ($operador eq '+') ? $primero + $segundo : $primero - $segundo;
+    # Luego realizamos las sumas y restas en el orden que aparecen
+    while ($operacion =~ /(-?\d+)(\.\d+)?([\+\-])(-?\d+)(\.\d+)?/) {
+        my ($primero, $operador, $segundo) = ($1, $3, $4);
+        if (defined $2){
+            $primero.=$2;
+        }
+        if (defined $5){
+            $segundo.=$5;
+        }
+        my $temporal = ($operador eq '+') ? $primero + $segundo : $primero - $segundo;
         $operacion =~ s/\Q$primero$operador$segundo\E/$temporal/;
     }
 
